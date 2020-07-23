5316aab969c25cc66cc50fbcd4e0fd30af45c1d7
andrebizeul <andybiz7401@gmail.com>
2020-07-23 08:43:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TheHeader></TheHeader>
+    <div class="d-flex flex-row">
+      <shop class="w-75"> </shop>
+      <cart class="w-25"></cart>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import Cart from './components/Cart/Cart.vue';
+import Shop from './components/Shop/Shop.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    Shop,
+    Cart
+  }
+}
+</script>
+
+<style>
+
+</style>
